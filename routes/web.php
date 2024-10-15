dc07f5f9b4a6638aa49cc330d21e991b1ccc6488
khang <vovykhag423@gmail.com>
2024-10-15 19:13:00 +0700

    outside

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,12 @@ use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\LocationController;
 use App\Http\Controllers\Admin\MapController;
+use App\Http\Controllers\Outside\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [HomeController::class, 'index']);
 Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login');
-Route::get('/admin/login', [AdminAuthController::class, 'index']) 
+Route::get('/admin/login', [AdminAuthController::class, 'index'])
     ->middleware('checkLoginAdmin')
     ->name('admin.loginView');
 
@@ -21,12 +19,12 @@ Route::group([
 ], function () {
     Route::get('/', [AdminController::class, 'index']);
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
-    Route::get('/map', [MapController::class , 'index'])->name('admin.map');
-    Route::get('/map/getAllLocation', [MapController::class , 'getAllLocation'])->name('admin.map.getAllLocation');
-    Route::post('/map/store', [MapController::class , 'store'])->name('admin.map.store');
+    Route::get('/map', [MapController::class, 'index'])->name('admin.map');
+    Route::get('/map/getAllLocation', [MapController::class, 'getAllLocation'])->name('admin.map.getAllLocation');
+    Route::post('/map/store', [MapController::class, 'store'])->name('admin.map.store');
 
-    Route::get('/list-location' ,[LocationController::class, 'index'])->name('admin.list-location');
-    Route::get('/getList' ,[LocationController::class, 'getList'])->name('admin.list-location.getList');
-    Route::post('/delete' ,[LocationController::class, 'delete'])->name('admin.list-location.delete');
+    Route::get('/list-location', [LocationController::class, 'index'])->name('admin.list-location');
+    Route::get('/getList', [LocationController::class, 'getList'])->name('admin.list-location.getList');
+    Route::post('/delete', [LocationController::class, 'delete'])->name('admin.list-location.delete');
     Route::get('admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
 });
